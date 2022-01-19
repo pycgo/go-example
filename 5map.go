949391c8fc3a919a2a924ec1ab384524140fa30d
xiaoxiao.zhang <xiaoxiao.zhang@linkflowtech.com>
2022-01-19 17:09:56 +0800

    function method interface

--- 5map.go
+++ 5map.go
@@ -1,14 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	m := make(map[string]int)
 	m["11"] = 1
 	m["12"] = 1
 	fmt.Println(m)
-
+	//循环读取元素
+	for k, v := range m {
+		fmt.Println(k, v)
+	}
 	delete(m, "11")
 	fmt.Println("after delete: ", m)
-
 }
