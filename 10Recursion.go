c3bf5c978d6d84359291464b0345d44d75533295
xiaoxiao.zhang <xiaoxiao.zhang@linkflowtech.com>
2022-01-20 17:02:56 +0800

    斐波那契数列

--- 10Recursion.go
+++ 10Recursion.go
@@ -1,11 +1,9 @@
 //递归  函数一直调用本身不断往前算
-
-//求 1*2*3*4* ...
-
 package main
 
 import "fmt"
 
+//求 1*2*3*4* ...
 func recursion(num int) int {
 	if num == 1 {
 		return 1
@@ -13,7 +11,16 @@ func recursion(num int) int {
 	return num * recursion(num-1)
 }
 
+//斐波那契数列
+func fib(n int) int {
+	if n < 2 {
+		return n
+	}
+	return fib(n-1) + fib(n-2)
+}
+
 func main() {
 	sum := recursion(4)
-	fmt.Print(sum)
+	fmt.Println("乘积 ", sum)
+	fmt.Print("斐波那契 ", fib(7))
 }
